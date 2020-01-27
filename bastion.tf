fbdb4a43fbe181b1c03770020d8a3ad96101ce3c
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-26 22:30:20 -0600

    versioning

--- bastion.tf
+++ bastion.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "bastion" {
-  ami             = ${data.aws_ami.centos7.id}
-  instance_type   = ${var.instance_type}
-  associate_public_ip_address = ${var.associate_public_ip_address}
-  key_name = ${aws_key_pair.deployer.key_name}
+    ami = "${data.aws_ami.centos7.id}"
+    instance_type   = "${var.instance_type}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
+  key_name = "${aws_key_pair.deployer.key_name}"
   security_groups = ["allow_ssh"]
   lifecycle{
     prevent_destroy = false
