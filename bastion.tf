d106e02447b8d4af033d41f018ecc3100f34c4ed
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-27 00:47:57 -0600

    sg

--- bastion.tf
+++ bastion.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion" {
   associate_public_ip_address = "${var.associate_public_ip_address}"
   key_name = "${aws_key_pair.deployer.key_name}"
   vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
-  vpc_id    = "${aws_vpc.main.id}"
+  subnet_id      = "${element(aws_subnet.public_subnets.*.id,count.index)}"
 
   lifecycle{
     prevent_destroy = false
