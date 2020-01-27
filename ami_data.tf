21128bef31301ed8f014d7dfb3f596f714607547
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-26 22:53:59 -0600

    data source removed

--- ami_data.tf
+++ ami_data.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "centos7" {
-  most_recent = true
-  owners = ["099720109477"]
-
-  filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-}
+#data "aws_ami" "centos7" {
+ # most_recent = true
+  #owners = ["099720109477"]
+#
+ # filter {
+  #  name = "name"
+   # values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  #}
+#}
