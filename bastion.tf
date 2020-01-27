5486f57b9b561e793acd4283c2fd7b7928d280e4
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-26 23:59:06 -0600

    sg

--- bastion.tf
+++ bastion.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
     instance_type   = "${var.instance_type}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
   key_name = "${aws_key_pair.deployer.key_name}"
-  security_groups = ["${allow_ssh}"]
+  security_groups = ["allow_ssh"]
   lifecycle{
     prevent_destroy = false
   }
