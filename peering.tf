35bd954cb29b1839b6bd227995c60724165a346a
Mohammad Karimi <mkarimidevops@gmail.com>
2020-01-27 15:28:37 -0600

    vpc

--- peering.tf
+++ peering.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_peering_connection" "foo" {
   #peer_owner_id = "${var.peer_owner_id}"
-  peer_vpc_id   = "vpc-906f9ffb"
+  peer_vpc_id   = "vpc-09060d0b3667d3e87"
   vpc_id        = "vpc-2ada9250"
   auto_accept   = true
 
